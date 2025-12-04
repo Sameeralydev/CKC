45463bb5d51c99e7ccb1d8a3cfe9d1dbbbdebf7f
Sameer <sameeraly2003@gmail.com>
2025-12-04 18:08:37 +0500

    contact us

--- src/Components/ContactUs/About/Index.jsx
+++ src/Components/ContactUs/About/Index.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect } from "react";
 import "./About.css";
-import About from "../../../assets/images/facilities/about.png";
-import About_one from "../../../assets/images/facilities/about-one.png";
-import About_two from "../../../assets/images/facilities/about-two.png";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { FaBuilding } from "react-icons/fa";
@@ -18,26 +15,27 @@ export const ContactSection = () => {
             <FaBuilding className="icon-orange" /> Head Office
           </h3>
 
-          <h2 className="office-name">FFS (HEAD OFFICE)</h2>
+          <h2 className="office-name">CKC (HEAD OFFICE)</h2>
 
           <p className="office-address">
-            Baraf Khana Chowk Misrial Road Rawalpindi   </p>
+            47 Rachna Block Allama Iqbal Town, Lahore, Pakistan, 54000   </p>
           <div className="phone-row">
             <FaPhoneAlt className="phone-icon" />
-            <span>051-5680007</span>
+            <span>0335 4677850
+</span>
           </div>
 
           <div className="email-row">
             <MdEmail className="email-icon" />
-            <span>info@ffs.edu.pk</span>
+            <span>ckcseniorcampus@gmail.com</span>
           </div>
         </div>
 
         {/* RIGHT SIDE GOOGLE MAP */}
         <div className="contact-map">
           <iframe
-            title="RILLS Map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3320.19232627058!2d74.2005!3d32.1694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391eead20ac42f1b%3A0x56e96365e6a85ee8!2sRILLS%20SCHOOL!5e0!3m2!1sen!2s!4v1700000000"
+            title="CKC Map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5456.456905168348!2d74.28240618326417!3d31.51203050375939!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3919030a59922d11%3A0xb268c975b602d932!2sCrescent%20Kids%20Campus%20High%20School!5e0!3m2!1sen!2s!4v1764852669637!5m2!1sen!2s"
             allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
